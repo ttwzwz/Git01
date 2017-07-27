d99b376be05ec311dadfc72117510737221b9dce
ttwzwz <757825226@qq.com>
2017-07-27 10:16:21 +0800

    冲突
    冲突

--- src/com/git/hello/Hello.java
+++ src/com/git/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("这是第一个git工程");
 		System.out.println("我已收到你的信息");
+		System.out.println("这是github上的冲突");
 	}
 
 }
